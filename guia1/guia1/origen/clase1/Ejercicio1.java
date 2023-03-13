cb50e085cd75604083b99793aaf43e52918ccfde
hernanromo <126093350+hernanromo@users.noreply.github.com>
2023-03-13 11:04:25 -0300

    cambiamos int numeroInicio

--- guia1/guia1/origen/clase1/Ejercicio1.java
+++ guia1/guia1/origen/clase1/Ejercicio1.java
@@ -3,7 +3,7 @@ package clase1;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		int numeroInicio = 7;
+		int numeroInicio = 5;
 		int numeroFin =10;
 		// Se deberían mostrar los números:
 		// 5,6,7,8,9,10,11,12,13,14
